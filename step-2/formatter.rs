eeab71054c1dd7dfedbe88282f5f0200bd152ebf
rehmanm <muhammad.rehman@ibexglobal.com>
2019-06-22 10:35:13 +0500

    formatter

---
+++ step-2/formatter.rs
@@ -0,0 +1,22 @@
+fn main(){
+
+    let x = 10;
+    println!("x = {}", x);
+
+    println!("{0}, this is {1} and {1}, this is {0}", "Alice", "Bob");
+
+    println!("{subject} {verb} {object}",
+        subject = "the quick brown fox",
+        object = "the brown fox",
+        verb = "jumps"
+    );
+
+    //Special formatter ':'
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+
+    println!("{number:>width$}", number=1, width=6);
+
+
+
+
+}
---
+++ step-2/step-2.rs
@@ -0,0 +1,20 @@
+fn main(){
+
+    // comments
+
+    /*
+        Multi line comments
+        Multi line comments 2
+    */
+
+    // variable decleration
+
+    let x = 5;
+    println!("value of 'x' is {}", x);
+
+    let y = 5 /* + 90  */ + 5;
+    println!("value of 'y' is {}", y); 
+
+
+
+}
